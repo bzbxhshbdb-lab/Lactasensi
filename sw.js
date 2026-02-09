d49171659cf9c826a2b64b77fd51f6c62a7c56f9
bzbxhshbdb-lab <bzbxhshbdb@gmail.com>
2026-02-09 09:24:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open("lacta").then(c=>c.addAll([
-"./","index.html","style.css","app.js"
-])))
+self.addEventListener("install", e=>{
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.open("lacta-v1").then(cache=>{
+      return cache.match(e.request).then(resp=>{
+        return resp || fetch(e.request).then(r=>{
+          cache.put(e.request, r.clone());
+          return r;
+        });
+      });
+    })
+  );
 });
